44db17964265485f30b26695ef5bb94f4132042a
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-03-01 16:33:23 +0800

    initial deployment test

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,7 @@ const config: GatsbyConfig = {
     title: `Portfolio-Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
+  pathPrefix: "/portfolio-site",
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-postcss",
