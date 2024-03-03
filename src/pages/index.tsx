db2aac7bf8a3d6e36199709709caebb6e28dab26
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-03-03 13:16:29 +0800

    Divide main page into smaller components, also added padding to intro text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,35 +63,13 @@ function App() {
         </button>
       </div>
     </SideBar>
+
     <div className="absolute left-20" id='landing'> {/* TODO: Terrible way of doing this ,change later*/}
       <Main>
 
-        <div className="m-24 w-auto h-[32rem] flex flex-col text-stone-50 bg-orange-400 rounded-3xl bg-opacity-20 justify-center items-center">
-          <p className=" text-3xl font-bold">Hi, welcome to my website.</p>
-          <p className=" text-1xl">Site is still under construction, but feel free to click the buttons in the sidebar to find out more about me!</p>
-        </div>
+        <Landing />
 
-
-
-
-        <div id="profile" />
-        <main className='bg-stone-700 flex flex-row w-auto h-[38rem] mx-32 mt-12 mb-16 relative bg-opacity-80 shadow-2xl rounded-xl'>
-          <StaticImage src="../../data/images/icon.png" layout="constrained" alt="Me" className='absolute left-4 mt-5 drop-shadow-2xl w-[45%] h-auto' />
-          <p className='absolute left-1/2 pt-16 text-white'>
-
-            <p className="pb-8 text-xl font-bold">I'm Yves Miguel Reyes</p>
-            <p className="pb-8 ">
-              I am a graduate from the University of Western Australia, majoring in Computer Science.
-            </p>
-            <p className="pb-8 ">
-              I've built various things, from websites to scripts.
-              I can program in C, C#, Java, Scala, Python and Lua.
-            </p>
-          </p>
-
-          <div className="absolute left-1/2 bottom-[12.5%] text-center text-white bg-blue-500 pl-2 pr-4 rounded-full transition ease-in-out delay-50 hover:scale-105 hover:bg-opacity-100 ">
-          </div>
-        </main>
+        <Profile />
 
 
         <div id='projects' />
@@ -122,3 +100,32 @@ function App() {
 }
 
 export default App
+
+function Landing({ }) {
+  return (<div className="m-24 w-auto h-[32rem] flex flex-col text-stone-50 bg-orange-400 rounded-3xl bg-opacity-20 justify-center items-center">
+    <p className=" text-3xl font-bold">Hi, welcome to my website.</p>
+    <p className=" text-1xl">Site is still under construction, but feel free to click the buttons in the sidebar to find out more about me!</p>
+  </div>);
+}
+
+function Profile({ }) {
+  return (<><div id="profile" />
+    <main className='bg-stone-700 flex flex-row w-auto h-[38rem] mx-32 mt-12 mb-16 relative bg-opacity-80 shadow-2xl rounded-xl'>
+      <StaticImage src="../../data/images/icon.png" layout="constrained" alt="Me" className='absolute left-4 mt-5 drop-shadow-2xl w-[45%] h-auto' />
+      <p className='absolute left-1/2 pt-16 text-white w-1/2 pr-8'>
+
+        <p className="pb-8 text-xl font-bold">I'm Yves Miguel Reyes</p>
+        <p className="pb-8 ">
+          I am a graduate from the University of Western Australia, majoring in Computer Science.
+        </p>
+        <p className="pb-8 ">
+          I've built various things, from websites to scripts.
+          I can program in C, C#, Java, Scala, Python and Lua.
+        </p>
+      </p>
+
+      <div className="absolute left-1/2 bottom-[12.5%] text-center text-white bg-blue-500 pl-2 pr-4 rounded-full transition ease-in-out delay-50 hover:scale-105 hover:bg-opacity-100 ">
+      </div>
+    </main></>);
+}
+
