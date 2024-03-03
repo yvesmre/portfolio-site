5656c9e041eab09cceb29606b9029d684d7d0997
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-03-03 19:29:08 +0800

    Dynamic div generation for separate paragraphs in intro JSON file

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -4,7 +4,7 @@ import Image from 'gatsby-image'
 
 function Profile() {
 
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
         introJson {
           text
@@ -22,19 +22,24 @@ function Profile() {
       }
    `);
 
-    return <div id="profile">
-        <main className='bg-stone-700 flex flex-row w-auto h-[38rem] mx-32 mt-12 mb-16 relative bg-opacity-80 shadow-2xl rounded-xl'>
-            <div className=" absolute mx-[2%] my-[5%] drop-shadow-2xl w-[45%] h-[45%]">
-                <Image fluid={data.introJson.image.childImageSharp.fluid} />
-            </div>
-            <p className='absolute left-1/2 pt-16 text-white w-1/2 pr-8'>
-                <p className="pb-8 text-xl font-bold">{data.introJson.introduction}</p>
-                <p className="pb-8">
-                    {data.introJson.text}
-                </p>
-            </p>
-        </main>
-    </div>;
+  const text = data.introJson.text
+
+  return <div id="profile">
+    <main className='bg-stone-700 flex flex-row w-auto h-[38rem] mx-32 mt-12 mb-16 relative bg-opacity-80 shadow-2xl rounded-xl'>
+      <div className=" absolute mx-[2%] my-[5%] drop-shadow-2xl w-[45%] h-[45%]">
+        <Image fluid={data.introJson.image.childImageSharp.fluid} />
+      </div>
+      <p className='absolute left-1/2 pt-16 text-white w-1/2 pr-8'>
+        <p className="pb-8 text-xl font-bold">{data.introJson.introduction}</p>
+        {data.introJson.text.map((text: any) => {
+          console.log(text)
+          return <p className="pb-8">
+            {text}
+          </p>
+        })}
+      </p>
+    </main>
+  </div>;
 }
 
 
