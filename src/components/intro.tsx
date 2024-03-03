a19e8cb152ef0c876c1c7165e363dfb91e515b24
TechnoStasis <yvesmiguelreyes1@gmail.com>
2024-03-03 21:40:41 +0800

    Added official images and splash - full text description to go

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -26,8 +26,8 @@ function Profile() {
 
   return <div id="profile">
     <main className='bg-stone-700 flex flex-row w-auto h-[38rem] mx-32 mt-12 mb-16 relative bg-opacity-80 shadow-2xl rounded-xl'>
-      <div className=" absolute mx-[2%] my-[5%] drop-shadow-2xl w-[45%] h-[45%]">
-        <Image fluid={data.introJson.image.childImageSharp.fluid} />
+      <div className="bg-stone-800 rounded-3xl absolute top-[6%] p-8 m-4 drop-shadow-2xl w-[45%] h-auto">
+        <Image fluid={data.introJson.image.childImageSharp.fluid} className="drop-shadow-2xl" />
       </div>
       <p className='absolute left-1/2 pt-16 text-white w-1/2 pr-8'>
         <p className="pb-8 text-xl font-bold">{data.introJson.introduction}</p>
