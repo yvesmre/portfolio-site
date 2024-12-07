1be2875203185329e51dd55e3eb6bb4a37c5d3e7
unknown <yvesmiguelreyes1@gmail.com>
2024-12-07 23:15:48 +0800

    Change style of portfolio shots, rounded corners are a no

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -8,7 +8,7 @@ import SideBar from './sidebar';
 
 const Portfolio = ({ title, imageData, description, url }: any) => (
     <div className="flex flex-col  justify-center items-center">
-        <div className="h-1/4 w-1/4 p-4 m-4 drop-shadow-2xl bg-gray-700 bg-opacity-75 rounded-3xl transition ease-in-out delay-100 hover:scale-110 hover:bg-opacity-100">
+        <div className="h-1/4 w-1/4 p-1 m-4 drop-shadow-2xl bg-gray-100 bg-opacity-75 transition ease-in-out delay-100 hover:scale-110 hover:bg-opacity-100">
             <p>  <Image fluid={imageData} /> </p>
         </div>
         <p className='text-center text-xl pt-4 pb-4 w-1/2 text-white'>{description}</p>
