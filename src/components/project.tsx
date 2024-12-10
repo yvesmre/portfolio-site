9772920fa9c7a97b45cd3b19a126fdce39f78065
Yves Miguel <7437383+yvesmre@users.noreply.github.com>
2024-12-10 20:47:52 +0800

    Add multiline support to projects as well

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -6,12 +6,16 @@ import { IoHomeOutline } from 'react-icons/io5';
 import SideBarIcon from './sidebar-icon';
 import SideBar from './sidebar';
 
-const Project = ({ title, imageData, description, url }: any) => (
-    <div className="flex flex-col  justify-center items-center">
+function Project({ title, imageData, description, url }: any) {
+
+    const descriptions = description.split('\n')
+
+    return <div className="flex flex-col  justify-center items-center">
         <div className="h-1/4 w-1/4 p-4 m-4 drop-shadow-2xl bg-gray-700 bg-opacity-75 rounded-3xl transition ease-in-out delay-100 hover:scale-110 hover:bg-opacity-100">
             <p>  <Image fluid={imageData} /> </p>
         </div>
-        <p className='text-center text-xl pt-2 pb-4 w-1/2 text-white'>{description}</p>
+
+        {descriptions.map((desc: String) => { return <p className='text-center text-xl pt-2 pb-4 w-1/2 text-white'>{desc}</p> })}
 
         {url ? <div className='bg-blue-500 w-auto m-6 rounded-full transition ease-in-out delay-50 hover:scale-105 hover:bg-opacity-100 '>
             <p className='p-2 px-4 text-xl underline hover:text-green-400 text-gray-300 drop-shadow-[0_50px_50px_rgba(0,0,0,255)] '>
@@ -23,6 +27,6 @@ const Project = ({ title, imageData, description, url }: any) => (
             </p>
         </div>}
     </div >
-);
+}
 
 export default Project
