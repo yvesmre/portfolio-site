6649564828e29af5addcd3fbdd6fd139810a51b8
unknown <yvesmiguelreyes1@gmail.com>
2025-02-05 00:53:34 +0800

    Add a date to the activity section

--- src/components/activity.tsx
+++ src/components/activity.tsx
@@ -7,6 +7,7 @@ function Activity() {
   const data = useStaticQuery(graphql`
     query {
         activityJson {
+          date
           text
           image {
             childImageSharp
@@ -30,10 +31,11 @@ function Activity() {
           <Image fluid={data.activityJson.image.childImageSharp.fluid} className="drop-shadow-2xl" />
         </div> */}
         <p className='text-white'>
-        <p className="pb-8 text-4xl font-bold"> What am I currently up to? </p>
+        <p className="pb-12 text-4xl font-bold"> What am I currently up to? </p>
+        <p className="pb-8 text-2xl underline"> {data.activityJson.date} </p>
           {data.activityJson.text.map((text: any) => {
             console.log(text)
-            return <p className="pb-8 text-2xl">
+            return <p className="pb-4 text-2xl">
               {text}
             </p>
           })}
