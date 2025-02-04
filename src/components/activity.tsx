dc7270c2f07dcd1b1f1fa0f1e96c87ce67003cf0
unknown <yvesmiguelreyes1@gmail.com>
2025-02-05 00:40:20 +0800

    Some unfinished activity status on the homepage

---
+++ src/components/activity.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Image from 'gatsby-image'
+
+function Activity() {
+
+  const data = useStaticQuery(graphql`
+    query {
+        activityJson {
+          text
+          image {
+            childImageSharp
+            {
+              fluid
+                {
+              ...GatsbyImageSharpFluid
+                }
+            }
+          }
+        }
+      }
+   `);
+
+  const text = data.activityJson.text
+
+  return <div id="profile">
+    <main className='bg-stone-700 flex flex-col w-auto h-[auto] mx-32 mt-12 mb-16 relative bg-opacity-80 shadow-2xl rounded-xl justify-center items-center'>
+      <div className="scale-[80%]">
+        {/* <div className="bg-stone-800 rounded-3xl  p-8  mb-8 drop-shadow-2xl w-[67%]">
+          <Image fluid={data.activityJson.image.childImageSharp.fluid} className="drop-shadow-2xl" />
+        </div> */}
+        <p className='text-white'>
+        <p className="pb-8 text-3xl font-bold"> What am I currently up to? </p>
+          {data.activityJson.text.map((text: any) => {
+            console.log(text)
+            return <p className="pb-8 text-xl">
+              {text}
+            </p>
+          })}
+        </p>
+      </div>
+    </main>
+  </div>;
+}
+
+
+export default Activity;
