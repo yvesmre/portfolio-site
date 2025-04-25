38c2e721b5b29ff0943e82d1102730fbca7d6b62
Yves Miguel <7437383+yvesmre@users.noreply.github.com>
2025-04-25 22:49:48 +0800

    Disable hover on mobile

--- src/templates/portfolio-template.tsx
+++ src/templates/portfolio-template.tsx
@@ -39,7 +39,7 @@ const PortfolioTemplate = ({ data }: any) => {
                         <main className='flex flex-row  flex-wrap w-[100%] sm:mx-[10%]'  >
                             {images.map(({ node: image }: any) => {
                                 // console.log(image.childImageSharp.fluid)
-                                return <div className="h-1/4 sm:w-1/4 min-w-56 sm:ml-0 p-2 m-4 drop-shadow-2xl bg-gray-300 bg-opacity-75 transition ease-in-out delay-100 hover:scale-110 hover:bg-opacity-100">
+                                return <div className="h-1/4 sm:w-1/4 min-w-56 sm:ml-0 p-2 m-4 drop-shadow-2xl bg-gray-300 bg-opacity-75 transition ease-in-out delay-100 md:hover:scale-110 md:hover:bg-opacity-100">
                                     <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={'Body image'} /> </div>
                             })}
 
